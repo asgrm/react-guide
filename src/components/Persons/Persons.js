cd34f005870c6aaf7862c133b2eec5c2978138f7
Andrii <andriigromakov@gmail.com>
2021-04-09 16:35:59 +0300

    done point 7.7

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = props => (
-  props.persons.map((item, i) => {
-    return <Person
+const persons = props => {
+  console.log('[Persons.js] render');
+  return props.persons.map((item, i) => {
+    return (
+    <Person
       key={item.id}
       name={item.name}
       age={item.age}
       click={() => props.clicked(i)}
       changed={(event)=>props.changed(event, item.id)}
       />
-  })
-);
+      );
+  });
+};
 
 export default persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,22 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+  componentWillMount() {
+    console.log('[App.js] UNSAFE_componentWillMount');
+  }
   state = {
     persons: [
       {id: 'f2db',name: 'asgrm', age: 28},
@@ -38,6 +54,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if(this.state.showPersons) {
@@ -51,6 +68,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           persons={this.state.persons}
           showPersons={this.state.showPersons}
           clicked={this.togglePersonHandler}
